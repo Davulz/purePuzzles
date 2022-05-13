805e256bd077915f063c73f8570da9a32797dace
Davulz <97291614+Davulz@users.noreply.github.com>
2022-05-13 09:59:40 +0200

    Add project files.

---
+++ purePuzzles/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+var metoder = new Metoder();
+
+Console.Write("Enter a line of text:");
+var Text =Console.ReadLine().ToLower();
+
+metoder.WordsLength( Text );
+metoder.CheckVowels( Text );
+
+Console.WriteLine($"Setningen er {metoder.Strlist.Length} ord lang.\nLengste ord er {metoder.WordLength} bokstaver langt, og er: {metoder.LongestWord}\nOrdet med flest vokaler har {metoder.NumberOfVowels}stk.");
---
+++ purePuzzles/metoder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+public class Metoder
+{
+    public int WordLength { get; set; }
+    public string LongestWord;
+    private readonly char[] _separator;
+    public string[] Strlist;
+    public int NumberOfVowels { get; set; }
+
+
+    public Metoder()
+    {
+        WordLength = 0;
+        LongestWord = "";
+        _separator = new char[] { ' ', '?', ',' };
+        NumberOfVowels = 0;
+    }
+
+    public void WordsLength(string text)
+    {
+        Strlist = text.Split(_separator);
+
+        foreach (var ord in Strlist)
+        {
+            if (ord.Length > WordLength)
+            {
+                WordLength = ord.Length;
+                LongestWord = ord;
+            }
+
+            if (ord.Length != WordLength)
+            {
+                return;
+            } 
+            
+            var wrdlist = LongestWord.Split(_separator);
+                var kopi = false;
+                foreach (var longWords in wrdlist)
+                {
+                    if (longWords == ord)
+                    {
+                        kopi = true;
+                    }
+
+                    if (!kopi)
+                    {
+                        LongestWord += " " + ord;
+                    }
+            }
+        }
+    }
+
+    public void CheckVowels(string text)
+    {
+        Strlist = text.Split(_separator);
+        var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' , 'æ', 'ø' , 'å' };
+
+
+        foreach (var i in Strlist)
+        {
+            var vowelsCount = i.Count(c => vowels.Contains(c));
+            if (vowelsCount > NumberOfVowels) NumberOfVowels = vowelsCount;
+        }
+    }
+
+
+
+
+}
+
+    
